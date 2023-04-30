1bf4ed009777bcf52e93ab0676ef30b13faaf938
Pallav Marwaha <pallav.marwah@gmail.com>
2023-04-30 21:58:14 +0530

    changed custom user admin class

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,10 +6,22 @@ from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 
-class CustomUser(UserAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+    """
+    Custom user admin for adding new fields from custom user model.
+    """
+
+    fieldsets = (
+        *UserAdmin.fieldsets,  # original form fieldsets, expanded
+        (  # new fieldset added on to the bottom
+            "Other info",  # group heading of your choice; set to None for a blank space instead of a header
+            {
+                "fields": ("is_student",),
+            },
+        ),
+    )
 
 
-admin.site.register(get_user_model(), CustomUser)
+admin.site.register(get_user_model(), CustomUserAdmin)
 admin.site.register(College)
 admin.site.register(Student)
