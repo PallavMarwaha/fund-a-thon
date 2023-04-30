76e4f8e14e8585b5a197ef19c7c047c523dc94b1
Pallav Marwaha <pallav.marwah@gmail.com>
2023-04-30 21:28:03 +0530

    fixed admin not hashing user password

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from .models import College, Student
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
-admin.site.register(get_user_model())
+
+class CustomUser(UserAdmin):
+    pass
+
+
+admin.site.register(get_user_model(), CustomUser)
 admin.site.register(College)
 admin.site.register(Student)
