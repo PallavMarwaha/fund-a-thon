92c08ce28aa3927bac864a89b74e3def88c3bfb5
Pallav Marwaha <pallav.marwah@gmail.com>
2023-05-05 00:01:49 +0530

    added photos section

--- frontend/src/pages/PostDetail.jsx
+++ frontend/src/pages/PostDetail.jsx
@@ -84,6 +84,50 @@ export function PostDetail() {
                                 <p className="pb-3 uk-text-meta">(Funds required)</p>
                             </div>
                         </div>
+
+                        <div>
+                            <div className="w-full bg-white shadow flex flex-col my-4 p-6">
+                                <p className="text-xl font-semibold pb-5">Photos</p>
+                                <div className="grid grid-cols-3 gap-3">
+                                    <img
+                                        className="hover:opacity-75"
+                                        src="https://source.unsplash.com/collection/1346951/150x150?sig=1"
+                                    />
+                                    <img
+                                        className="hover:opacity-75"
+                                        src="https://source.unsplash.com/collection/1346951/150x150?sig=2"
+                                    />
+                                    <img
+                                        className="hover:opacity-75"
+                                        src="https://source.unsplash.com/collection/1346951/150x150?sig=3"
+                                    />
+                                    <img
+                                        className="hover:opacity-75"
+                                        src="https://source.unsplash.com/collection/1346951/150x150?sig=4"
+                                    />
+                                    <img
+                                        className="hover:opacity-75"
+                                        src="https://source.unsplash.com/collection/1346951/150x150?sig=5"
+                                    />
+                                    <img
+                                        className="hover:opacity-75"
+                                        src="https://source.unsplash.com/collection/1346951/150x150?sig=6"
+                                    />
+                                    <img
+                                        className="hover:opacity-75"
+                                        src="https://source.unsplash.com/collection/1346951/150x150?sig=7"
+                                    />
+                                    <img
+                                        className="hover:opacity-75"
+                                        src="https://source.unsplash.com/collection/1346951/150x150?sig=8"
+                                    />
+                                    <img
+                                        className="hover:opacity-75"
+                                        src="https://source.unsplash.com/collection/1346951/150x150?sig=9"
+                                    />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </article>
 
