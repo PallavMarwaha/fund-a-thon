4f2bbbe83ca0ebff8b2f1c0829e992c4862a087f
Pallav Marwaha <pallav.marwah@gmail.com>
2023-05-05 16:12:20 +0530

    create create_fundraiser view skeleton

--- fundraisers/views.py
+++ fundraisers/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .decorators import is_student_required
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
+from rest_framework.authentication import (
+    SessionAuthentication,
+    TokenAuthentication,
+)
 
-# Create your views here.
+
+@api_view(["POST"])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@is_student_required
+def create_fundraiser(request):
+    return Response({"detail": "Hello world"})
