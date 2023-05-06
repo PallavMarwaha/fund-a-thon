b9dc771250dfe93f506c08141e411141ee13fe38
Pallav Marwaha <pallav.marwah@gmail.com>
2023-05-06 15:51:03 +0530

    added link for fundraiser list template

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -202,6 +202,11 @@ export function Home() {
                                 src="https://source.unsplash.com/collection/1346951/150x150?sig=9"
                             />
                         </div>
+                        <Link
+                            to={routes.fundraisers.base}
+                            className="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
+                            See new fundraiser
+                        </Link>
                     </div>
                 </aside>
             </div>
