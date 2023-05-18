80cf304dfaf6541dacafb60930f973c647d492d4
Pallav Marwaha <pallav.marwah@gmail.com>
2023-05-19 02:07:24 +0530

    added check for login and removed other photos in edit fundraiser

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate, redirect } from "react-router-dom";
 import { useSignIn, useIsAuthenticated } from "react-auth-kit";
 import axios from "axios";
 import { useEffect, useState, useLayoutEffect } from "react";
+import { toast } from "react-toastify";
 
 export function Login() {
     const signIn = useSignIn();
@@ -45,6 +46,7 @@ export function Login() {
             })
             .catch((error) => {
                 console.log(error);
+                toast.error("Please try again.");
             });
     };
 
@@ -69,7 +71,7 @@ export function Login() {
                                     id="username"
                                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     placeholder="username@123"
-                                    required=""
+                                    required
                                     onChange={(e) =>
                                         setFormData((prevState) => {
                                             return {
@@ -92,7 +94,7 @@ export function Login() {
                                     id="password"
                                     placeholder="••••••••"
                                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    required=""
+                                    required
                                     onChange={(e) =>
                                         setFormData((prevState) => {
                                             return {
